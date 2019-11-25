2c66285e18505441f30342e0b3232a4a4c5dfad5
John Marcao <john.marcao@gmail.com>
2019-11-24 23:33:05 -0500

    Big FPS improvment, immplemented batching and downsampling. Current bug: Normals are not being properly calculated. Also moved to OBJ, plan to add one more step to allow conversion to GLTF.

--- mesh_construction/src/mesh_construct_node.cpp
+++ mesh_construction/src/mesh_construct_node.cpp
@@ -11,12 +11,25 @@
 #include <pcl/features/normal_3d.h>
 #include <pcl/surface/gp3.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/surface/poisson.h>
+#include <pcl/io/obj_io.h>
+#include <pcl/filters/uniform_sampling.h>
 #include <string.h>
 #include <sstream>
 #include <iomanip>
-#include <pcl/io/vtk_io.h>
+
+
+// Define these only in *one* .cc file.
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+// #define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
+#include "tiny_gltf.h"
+
+using namespace tinygltf;
 
 static int iteration = 0;
+
 static pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals = 0;
 static pcl::PointCloud<pcl::PointXYZRGB>::Ptr incoming_cloud = 0;
 static pcl::PointCloud<pcl::Normal>::Ptr normals = 0;
@@ -26,51 +39,82 @@ static pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr kdtree = 0;
 std::vector<std::string> class_descriptions;
 std::string key;
 
+// Forward Decl.
+void ReducePointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud);
+void WriteMeshToGLTF(pcl::PolygonMesh& mesh);
+void PointCloudToMesh(
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+	pcl::PolygonMesh& mesh);
+
 // this is registered every image sent
 void PointCloud2Callback(const sensor_msgs::PointCloud2& msg)
 {
 	ROS_INFO("Got message!");
 
 	// Init static variables
-	static pcl::PCLPointCloud2* conv_cloud = new pcl::PCLPointCloud2;
 	static pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-	static pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-	static pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
-	static pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-	static pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGBNormal>);
+	static pcl::PointCloud<pcl::PointXYZRGB>::Ptr tmp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
 	//
 	// Example code below from http://www.pointclouds.org/documentation/tutorials/greedy_projection.php
 	//
 
-	// Note, moveToPCL destroys the message structure.
-	pcl_conversions::toPCL(msg, *conv_cloud);
+	// Conert to proper format. This destroys the contents of the msg object!
+	pcl::fromROSMsg(msg, *tmp_cloud);
+	*cloud += *tmp_cloud;
 
-	// Now move to totally independent PCL Functions
-	// ROS dependence stops here
-	pcl::fromPCLPointCloud2 (*conv_cloud, *cloud);
+	// Write out every 20 cycles to reduce load.
+	iteration++;
+	if((iteration % 20) == 0) {
+		ROS_INFO("generating output!!");
+		pcl::PolygonMesh mesh;
+		ReducePointCloud(cloud);
+		PointCloudToMesh(cloud, mesh);
+		WriteMeshToGLTF(mesh);
+		cloud->clear(); // Don't reuse points after outputting to model once.
+	}
+}
+
+// Takes an input mesh and writes it to a file with an incrementing ID
+void WriteMeshToGLTF(pcl::PolygonMesh& mesh)
+{
+	static int mesh_count = 0;
+	std::string filename = "mesh_" + std::to_string(mesh_count++) + ".obj";
+	pcl::io::saveOBJFile(filename, mesh);
+}
+
+// Accepts a point cloud and constructs a surface using
+// greedy projection triangles. Still need to play with some other algorithms
+// to see what is most performant (plus what is best ported to GPU).
+void PointCloudToMesh(
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+	pcl::PolygonMesh& mesh)
+{
+	static pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+	static pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGBNormal>);
+	static pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+	static pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
 
 	// Normal estimation
 	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> n;
 	tree->setInputCloud(cloud);
 	n.setInputCloud(cloud);
 	n.setSearchMethod(tree);
-	n.setKSearch (20);
+	n.setKSearch(20);
 	n.compute(*normals);
 
 	// Concatenate the XYZ and normal fields
-	pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+	pcl::concatenateFields(*cloud, *normals, *cloud_with_normals);
 
 	// Create search tree
-	tree2->setInputCloud (cloud_with_normals);
+	tree2->setInputCloud(cloud_with_normals);
 
-	// Initialize objects
+	// Initialize Reconstruction Algo
 	pcl::GreedyProjectionTriangulation<pcl::PointXYZRGBNormal> gp3;
-	pcl::PolygonMesh triangles;
 
 	// Set typical values for the parameters
-	gp3.setSearchRadius (0.025);
-	gp3.setMu (2.5);
+	gp3.setSearchRadius(0.025);
+	gp3.setMu(2.5);
 	gp3.setMaximumNearestNeighbors (100);
 	gp3.setMaximumSurfaceAngle(M_PI/4);   // 45 degrees
 	gp3.setMinimumAngle(M_PI/18);         // 10 degrees
@@ -78,17 +122,25 @@ void PointCloud2Callback(const sensor_msgs::PointCloud2& msg)
 	gp3.setNormalConsistency(false);
 
 	// Get result
-	gp3.setInputCloud (cloud_with_normals);
-	gp3.setSearchMethod (tree2);
-	gp3.reconstruct (triangles);
+	gp3.setInputCloud(cloud_with_normals);
+	gp3.setSearchMethod(tree2);
+	gp3.reconstruct(mesh);
+}
 
-	// Additional vertex information
-	std::vector<int> parts = gp3.getPartIDs();
-	std::vector<int> states = gp3.getPointStates();
+// Filter out unimportant points in the cloud.
+void ReducePointCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
+{
+	// Plan: On GPU, sort by x, y, then z.
+	// Then filter out points that are identical within +- some range
 
-	iteration++;
-	std::string filename = "mesh_" + std::to_string(iteration) + ".vtk";
-	//pcl::io::saveVTKFile (filename, triangles);
+	// CPU Impl.
+	pcl::UniformSampling<pcl::PointXYZRGB> us;
+	us.setRadiusSearch(0.01f); // Experiment or set to 1/1000 of min/max
+	us.setInputCloud(cloud);
+	us.filter(*cloud);
+
+	// For GPU imple, take UniformSampling code and parallelize by Voxel.
+	// Straightforward but should provide big improvement.
 }
 
 
