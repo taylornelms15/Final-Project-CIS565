f0bb2b107433372a45eea9b0d6d8b6f409b3ecdd
Eric Micallef <micallef@seas.upenn.edu>
2019-12-01 15:56:45 -0500

    changed file path

--- object_detection/utils/filesystem.cpp
+++ object_detection/utils/filesystem.cpp
@@ -48,8 +48,8 @@ std::string locateFile( const std::string& path, std::vector<std::string>& locat
 	}
 
 
-	locations.push_back("/home/slothjet/CIS565/droneMoM_ws/src/Final-Project-CIS565/deep_learning_pipeline/data");
-	locations.push_back("/home/slothjet/CIS565/droneMoM_ws/src/Final-Project-CIS565/deep_learning_pipeline/data/");
+	locations.push_back("/home/slothjet/CIS565/droneMoM_ws/src/Final-Project-CIS565/object_detection/data");
+	locations.push_back("/home/slothjet/CIS565/droneMoM_ws/src/Final-Project-CIS565/object_detection/data/");
 
 	// check each location until the file is found
 	const size_t numLocations = locations.size();
